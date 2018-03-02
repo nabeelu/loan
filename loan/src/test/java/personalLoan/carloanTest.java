0d6d3013a2979fef07c9b6e0441c3245fae66e9c
nandoorath001 <nandoorath001@nandoorath00170.us.nam.ad.pwcinternal.com>
2018-03-02 19:44:57 +0530

    change car

--- loan/src/test/java/personalLoan/carloanTest.java
+++ loan/src/test/java/personalLoan/carloanTest.java
@@ -3,11 +3,7 @@ package personalLoan;
 import org.testng.annotations.Test;
 
 public class carloanTest {
-	@Test
-	public void carstart(){
-		
-		System.out.println("I am loan for car");
-	}
+	
 	
 	@Test
 	public void carend(){
